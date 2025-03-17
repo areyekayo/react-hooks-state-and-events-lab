a9a3a91aa3c99800967cc51ca801c850a932db73
Riko <riko.fluchel@gmail.com>
2025-03-17 08:02:45 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+	const [appMode, setAppMode] = useState(false)
+	// replace 'false' with a state variable that can be toggled between true and false
+	// this will be used for the Dark Mode Toggle feature
+	const appClass = appMode ? "App dark" : "App light"
+	const toggleText = appMode ? "Dark Mode" : "Light Mode"
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+	function handleModeClick() {
+		setAppMode((appMode) => !appMode);
+	}
 
-  return (
-    <div className={appClass}>
-      <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
+	return (
+		<div className={appClass}>
+		<header>
+			<h2>Shopster</h2>
+			<button onClick={() => handleModeClick()}>{toggleText}</button>
+		</header>
+		<ShoppingList items={itemData} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
+	const [cart, setIsInCart] = useState(true);
+
+	const cartClass = cart ? "" : "in-cart" ;	
+	const cartButtonText = cart ? "Add to Cart" : "Remove From Cart";
+
+	function handleClick() {
+		setIsInCart((isInCart) => !isInCart);
+	}
+
+	return (
+		<li className={cartClass}>
+		<span>{name}</span>
+		<span className="category">{category}</span>
+		<button className="add" onClick={() => handleClick()}>{cartButtonText}</button>
+		</li>
+	);
 }
 
 export default Item;
